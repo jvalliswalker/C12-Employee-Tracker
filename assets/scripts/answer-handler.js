e4fe8868788e361ff04c20182fc8a47be498732b
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-09-11 15:58:25 -0700

    Change question-router to answer-handler, export class only

--- assets/scripts/answer-handler.js
+++ assets/scripts/answer-handler.js
@@ -78,48 +78,4 @@ class AnswerHandler {
   }
 }
 
-const questionsMain = [
-  {
-    name: 'main',
-    message: 'Please select an option',
-    type: 'list',
-    choices: [
-      "View all departments",
-      "View all roles",
-      "View all employees",
-      "Add a department",
-      "Add a role",
-      "Add an employee",
-      "Update an employee role",
-      "Exit"
-    ]
-  }
-];
-
-function promptMain(pool){
-
-  // Begin prompt for main "menu"
-  inquirer.prompt(questionsMain)
-    .then(answerObject => {
-      // Initiatize handler from answers and pool
-      const answerHandler = new AnswerHandler(answerObject.main, pool);      
-      return answerHandler;
-    })
-    .then(answerHandler => {
-      if(answerHandler.followupQuestions){
-        return answerHandler.promptFollowup();
-      }
-      else {
-        return answerHandler;
-      }
-    })
-    .then(answerHandler => {
-      return answerHandler.runCRUD();
-    })
-    .then(answerHandler => {
-      answerHandler.displayCRUDResults();
-      promptMain(pool);
-    })
-  }
-
-module.exports = promptMain;
+module.exports = AnswerHandler;
