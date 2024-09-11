2bc52255e1f1ad4fe6b7dae336d7f7896d637fcc
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-09-11 15:54:49 -0700

    Add class, sort out callback hell, working model for non-question answers

--- assets/scripts/question-routing.js
+++ assets/scripts/question-routing.js
@@ -1,58 +1,125 @@
 const inquirer = require("inquirer");
+const {promisify} = require('util');
 
-function questionRouter(pool){
+class AnswerHandler {
+  constructor(answer, pool){
+    this.answer = answer;
+    this.pool = pool;
+    this.followupQuestionData;
+    this.answerRouter = this.router[answer];
+    this.CRUDStatement = this.answerRouter.statement;
+  }
 
-  const main = [
-    {
-      name: 'main',
-      message: 'Please select an option',
-      type: 'list',
-      choices: [
-        "View all departments",
-        "View all roles",
-        "View all employees",
-        "Add a department",
-        "Add a role",
-        "Add an employee",
-        "Update an employee role",
-      ]
+  router = {
+    "View all departments": {
+      statement: 'SELECT id AS "Id", name as "Department Name" FROM departments'
+    },
+    "View all roles": {
+      statement: (
+        'SELECT r.id AS "Role Id", r.title AS "Title", r.salary AS "Salary", d.name AS "Department Name"'+
+        'FROM roles r JOIN departments d ON r.department_id = d.id'
+      )
+    },
+    "View all employees": {
+      statement: (
+        'SELECT e.id AS "Employee Id", e.first_name AS "First Name", e.last_name AS "Last Name", '+
+        'r.title AS "Role", ee.first_name AS "Manager First Name", ee.last_name AS "Manager Last Name" '+
+        'FROM employees e '+
+        'INNER JOIN roles r ON e.role_id = r.id '+
+        'LEFT JOIN employees ee ON '+
+        'e.manager_id = ee.id'
+      )
+    },
+    'Add a department': {
+      questions: [
+        {
+          name: 'test',
+          message: 'Test question'
+        }
+      ],
+      statement: 'INSERT INTO departments VALUES'
+    },
+    'Add a role': {
+      // questions: [],
+      statement: 'INSERT INTO roles VALUES'
+    },
+    'Add a employee': {
+      // questions: [],
+      statements: 'INSERT INTO'
+    },
+    "Update an employee role": {
+      // questions: [],
+      statement: ''
     }
-  ]
-  
-  const queryRouter = {
-    'View all departments': 'SELECT id, name FROM departments',
-    'View all roles': (
-      'SELECT r.id AS "Role Id", r.title AS "Title", r.salary AS "Salary", d.name AS "Department Name"'+
-      'FROM roles r JOIN departments d ON r.department_id = d.id'
-    ),
-    'View all employees': (
-      'SELECT e.id, e.first_name, e.last_name, r.title, ee.first_name, ee.last_name '+
-      'FROM employees e '+
-      'INNER JOIN roles r ON e.role_id = r.id '+
-      'LEFT JOIN employees ee ON '+
-      'e.manager_id = ee.id'
-    )
-  }
-  
-  function renderData(queryString, pool){
-  
-    pool.query(queryString).then(data => {
-      const { rows } = data;
-  
-      console.log(rows);
-    });
+  };
+
+  get followupQuestions() {
+    return this.answerRouter.questions;
   }
 
-  renderData(queryRouter['View all roles'], pool);
+  promptFollowup(){
+    return inquirer.prompt(this.followupQuestions)
+      .then((answers) => {
+        this.followupAnswers = answers;
+        return this;
+      });
+  }
+
+  async runCRUD() {
+    return this.pool.query(this.CRUDStatement)
+      .then((data) => {
+        this.CRUDStatementData = data.rows;
+        return this;
+      });
+  }
+
+  displayCRUDResults() {
+    console.table(this.CRUDStatementData);
+  }
 }
 
-// function prettifyData(rowData){
+const questionsMain = [
+  {
+    name: 'main',
+    message: 'Please select an option',
+    type: 'list',
+    choices: [
+      "View all departments",
+      "View all roles",
+      "View all employees",
+      "Add a department",
+      "Add a role",
+      "Add an employee",
+      "Update an employee role",
+      "Exit"
+    ]
+  }
+];
 
-//   const tableColumns = {
-//     'departments': ['id','name'],
-//     'roles': ['id','title','salary'],
-//     // 'employees'
-//   }
-// }
+function promptMain(pool){
 
-module.exports = questionRouter;
+  // Begin prompt for main "menu"
+  inquirer.prompt(questionsMain)
+    .then(answerObject => {
+      // Initiatize handler from answers and pool
+      const answerHandler = new AnswerHandler(answerObject.main, pool);      
+      return answerHandler;
+    })
+    .then(answerHandler => {
+      if(answerHandler.followupQuestions){
+        return answerHandler.promptFollowup();
+      }
+      else {
+        return answerHandler;
+      }
+    })
+    .then(answerHandler => {
+      return answerHandler.runCRUD();
+    })
+    .then(answerHandler => {
+      answerHandler.displayCRUDResults();
+      promptMain(pool);
+    })
+  }
+
+module.exports = promptMain;
