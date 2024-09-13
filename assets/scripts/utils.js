b2ffc97681d6d6127279b54a596cfd3bb7378ea4
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-09-12 19:24:44 -0700

    Add comments, apply formatter

--- assets/scripts/utils.js
+++ assets/scripts/utils.js
@@ -1,28 +1,32 @@
-import fs from 'fs';
+import fs from "fs";
 
-const writeFile = (filePath, data, writeType='append') => {
-  if(writeType == 'append'){
-    fs.appendFileSync(filePath, data)
+// Create custom write-to-file function
+// with append and overwrite behavior
+const writeFile = (filePath, data, writeType = "append") => {
+  // Call fs.appendFileSync with data
+  if (writeType == "append") {
+    fs.appendFileSync(filePath, data);
   }
-  else if (writeType == 'overwrite'){
-    fs.writeFileSync(filePath, data,)
+  // Call fs.writeFileSync with data
+  else if (writeType == "overwrite") {
+    fs.writeFileSync(filePath, data);
   }
+  // Throw error if invalid write type passed
   else {
     throw new Error("Valid writeType values are: append, overwrite");
   }
-}
+};
 
+// Create cstom read=file function with utf-8 encoding
 const readFile = (filePath) => {
-  return fs.readFileSync(filePath, 'utf8', (error, data) => {
-    if(error){
+  return fs.readFileSync(filePath, "utf8", (error, data) => {
+    if (error) {
       console.log(error);
       return;
+    } else {
+      return data;
     }
-    else{      
-        return data;
-    }
-  })
-}
+  });
+};
 
-export { readFile, writeFile }
-// module.exports = { readFile, writeFile }
+export { readFile, writeFile };
